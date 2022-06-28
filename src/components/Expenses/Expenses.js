a3ca5f45faed35e82c0e77a7961f0bdf3bdc7e42
Fiora <fioratanbeilun@gmail.com>
2022-06-28 22:58:33 +0800

    conditional return expenses based on selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card"
 import './Expenses.css'
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList'
 
 const Expense = (props) => {
   const [year, setYear] = useState('2021')
@@ -20,14 +20,7 @@ const Expense = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={year} onYearSelect={onYearSelectHandler} />
-        {filteredExpenses.map((expense) =>
-        (<ExpenseItem
-          key={expense.id} // key is a prop that can be added to ANY component (custom or built in)
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />))}
-
+        <ExpensesList expenses={filteredExpenses}></ExpensesList>
       </Card>
     </div>
   );
